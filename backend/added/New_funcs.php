4e927341026acb648677b0e96f03a8dfe88e8f66
0-Reker-0 <147882553+0-Reker-0@users.noreply.github.com>
2024-07-29 20:12:57 +0300

    Update New_funcs.php

--- backend/added/New_funcs.php
+++ backend/added/New_funcs.php
@@ -13,6 +13,9 @@ class New_funcs
      */
     public static function select_cars(object $db, array $data):int
     {
+        
+        if($data['dayImput'] == '')
+            return false;
         $val = 0;
         foreach($data as $key => $record){
             if($key != 'product' && $key != 'dayImput')
